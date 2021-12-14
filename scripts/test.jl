022d9d2d59a704d7f9bda4fc63dd4df73b2586f5
Yamacia <mathieu.nguyen.1@etu.sorbonne-universite.fr>
2021-12-14 12:30:55 +0100

    LaTeX modification (numeric analyzation)

--- scripts/test.jl
+++ scripts/test.jl
@@ -16,20 +16,8 @@ function mass_balance_constants(xc, yc) # Permet de récupérer toutes les valeu
     Xc, Yc   = [Float32(x) for x=xc,y=yc], [Float32(y) for x=xc,y=yc]
     Yc2      = Yc .- minimum(Yc); Yc2 .= Yc2/maximum(Yc2)
     grad_b   = (1.3517 .- 0.014158.*(lat_min.+Yc2*(lat_max-lat_min)))./100.0.*0.91 # Mass Bal. gradient, from doi: 10.1017/jog.2016.75
-    z_ELA    = 1300.0 .- Yc2*300.0                                 # Educated guess for ELA altitude
+    z_ELA    = 2000.0 .- Yc2*300.0                                 # Educated guess for ELA altitude
     return grad_b, z_ELA, b_max
-end
-
-function as_geoarray(A, template::AbstractGeoArray; name=nothing, staggerd=false)
-    @assert length(size(A))==2
-    dd = if staggerd
-        x, y = dims(template)
-        (X( 0.5*(val(x)[2:end] + val(x)[1:end-1]), mode=x.mode, metadata=x.metadata),
-         Y( 0.5*(val(y)[2:end] + val(y)[1:end-1]), mode=y.mode, metadata=y.metadata))
-    else
-        dims(template)
-    end
-    return GeoArray(A, dims=dd, name=name)
 end
 
 @views function diffusion_1D(; do_visu=true)
@@ -46,14 +34,13 @@ end
     nout     = 50 #200         # error check frequency
     tolnl    = 1e-6            # nonlinear tolerance
     epsi     = 1e-4            # small number
-    damp     = 0.65            # Pour converger plus vite (on l'a arbitrairement mis à 0.65)
+    damp     = 0.60            # Pour converger plus vite (on l'a arbitrairement mis à 0.65)
     dtausc   = 0.75/3.0        # iterative dtau scaling (on l'a arbitairement mis à 0.75/3, peut changer)
     # derived physics
     cfl    = 0                 # Confition de stzbilite
     a      = 2.0*a0/(npow+2)*(rho_i*g)^npow*s2y  #Viscosité de la glace
     nx = 96 # Nombre de valeurs en x
     # Array allocation (Chaque tableau à une taille dépendante du calcul qu'on y fait)
-    Vx     = zeros(nx-1)
     qH     = zeros(nx-2) # Flux
     dHdt   = zeros(nx-3)
     dtau   = zeros(nx-3)
@@ -96,25 +83,25 @@ end
     println(" starting iteration loop:")
     iter = 1; err = 2*tolnl
     while err>tolnl && iter<itMax # On sort lorsque l'erreur est en dessous de la tolérance (ou trop d'itérations)
-        Err .= H # On remet à jour l'erreur
-        M     .= min.(grad_b.*(S .- z_ELA), b_max) # Abblation
-        dSdx .= diff(S)/dx 
-        D     .= a*av(H).^(npow+2) .*dSdx.^(npow-1) # Coefficient de diffusion NON LINEAIRE
-        qH         .= .-av(D).*diff(S[1:end-1])/dx  # flux -> négatif lorsqu'on grimpe la courbe
-        ResH  .= .-(diff(qH)/dx .+ inn(M[1:end-1])) 
-        dtau  .= dtausc*min.(10.0, cfl./(epsi .+ av(D[2:end])))
-        dHdt     .= ResH + damp.*dHdt         # Ajout du damp pour la convergence
-        H[2:end-2] .= max.(0.0,H[2:end-2] .+ dtau.*dHdt)  # Permet de mettre le niveau de la mer à 0
+        Err         .= H                                                # On remet à jour l'erreur
+        M           .= min.(grad_b.*(S .- z_ELA), b_max)                # Abblation
+        dSdx        .= diff(S)/dx 
+        D           .= a*av(H).^(npow+2) .*dSdx.^(npow-1)               # Coefficient de diffusion NON LINEAIRE
+        qH          .= .-av(D).*diff(S[1:end-1])/dx                     # flux -> négatif lorsqu'on grimpe la courbe
+        ResH        .= .-(diff(qH)/dx .+ inn(M[1:end-1])) 
+        dtau        .= dtausc*min.(10.0, cfl./(epsi .+ av(D[2:end])))
+        dHdt        .= ResH + damp.*dHdt                                # Ajout du damp pour la convergence
+        H[2:end-2]  .= max.(0.0,H[2:end-2] .+ dtau.*dHdt)               # Permet de mettre le niveau de la mer à 0
         # apply mask (a very poor-man's calving law)
         H[Mask.==0] .= 0.0
         # update surface
-        S     .= B .+ H # Mise à jour de la surface
+        S           .= B .+ H                                           # Mise à jour de la surface
         # error check
         if mod(iter, nout)==0 # Calcul de l'erreur tous les nout temps
-            @printf("dSdx = %f, S = %f, D = %f, qH = %f, ResH = %f, H = %f, dtau = %f", dSdx[50], S[50], D[50], qH[50], ResH[50], H[50], dHdt[50])
+            # @printf("dSdx = %f, S = %f, D = %f, qH = %f, ResH = %f, H = %f, dtau = %f", dSdx[50], S[50], D[50], qH[50], ResH[50], H[50], dHdt[50])
             Err .= Err .- H
             err = norm(Err)/length(Err) # Length = 96
-            @printf(" Err = %f, iter = %d, error = %1.2e \n", Err[50], iter, err)
+            # @printf(" Err = %f, iter = %d, error = %1.2e \n", Err[50], iter, err)
             if isnan(err)
                 error("""NaNs encountered.  Try a combination of:
                             decreasing `damp` and/or `dtausc`, more smoothing steps""")
@@ -123,9 +110,7 @@ end
         iter += 1
     end
 
-    # compute velocities
-    Vx .= -D./(av(H) .+ epsi).*dSdx
-    # return as GeoArrays
+    # Affiche la surface de la glace avant et après la simulation, ainsi que le niveau du lit rocheux
     if do_visu
         display(plot(xc, H0, linewidth=3, label="H initial")); display(plot!(xc, H, legend=true, framestyle=:box, linewidth=3,label="H apres diffusion", xlabel="lx", ylabel="H", title="iceflow 1D (nt=$nout, iters=$iter)"))
         display(plot!(xc, B, linewidth=3, label="lit rocheux"));
@@ -133,8 +118,5 @@ end
 end
 
 diffusion_1D(; do_visu = true)
-
-
-
 
 # ------------------------------------------------------------------------------
